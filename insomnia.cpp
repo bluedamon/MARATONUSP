3094104220835fc7d31dad20715343b5275993de
Damon <damon@usp.br>
2023-10-13 15:45:49 -0300

    CODEFORCES: A - Insomnia cure

--- insomnia.cpp
+++ insomnia.cpp
@@ -6,11 +6,10 @@ int main(){
     int k, l, m, n, d;
     cin >> k >> l >> m >> n >> d;
     int count = 0;
-    if(k <= d || l <= d || m <= d || n <= d)
-        while(d){
-            if(d % k == 0 || d % l == 0 || d % m == 0|| d % n == 0) count++;
-            d--;
-        } 
+    while(d){
+        if(d % k == 0 || d % l == 0 || d % m == 0|| d % n == 0) count++;
+        d--;
+    } 
     cout << count;
     return 0;
 }
