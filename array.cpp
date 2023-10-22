6622a3883a1436152f45f196ad0d94d045274c01
Damon <damon@usp.br>
2023-10-22 02:40:02 -0300

    CODEFORCES: A - Array

--- array.cpp
+++ array.cpp
@@ -1,4 +1,8 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main(){
@@ -14,18 +18,47 @@ int main(){
         arr.push_back(num);
     }
     sort(arr.begin(), arr.end());
-    int i = 0;
-    while(i < countNegatives){
-        cout << countNegatives << " " << arr.at(i) << " ";
-        i++;
-    } 
-    cout << endl;
-    int j = countNegatives + 1;
-    while(j < arr.size()){
-        cout << arr.size() - countNegatives - 1 << " " << arr.at(j) << " ";
-        j++;
-    } 
-    cout << endl;
-    cout << 1 << " " << 0;
+
+    if(countNegatives % 2 == 0){
+        int negatives = countNegatives - 3;
+        if(countNegatives == 2) negatives = 1;
+        cout << negatives << " ";
+        int i = 0;
+        while(i < negatives){
+            cout << arr.at(i) << " ";
+            i++;
+        }
+        cout << endl;
+        int positives = arr.size() - negatives - 2;
+        cout << positives << " ";
+        int j = negatives + 1;
+        while(j < arr.size()){
+            arr.at(j) != 0 ? cout << arr.at(j) << " " : cout << "";
+            j++;
+        } //-3 -2 0 4 5
+        cout << endl;        
+        cout << arr.size() - negatives - positives << " ";
+        cout << arr.at(countNegatives - 1) << " " << arr.at(countNegatives);
+    } else{
+        int negatives = countNegatives - 2;
+        if(countNegatives == 1) negatives = 1;
+        cout << negatives << " ";
+        int i = 0;
+        while(i < negatives){
+            cout << arr.at(i) << " ";
+            i++;
+        }
+        cout << endl;
+        int positives = arr.size() - negatives - 1;
+        cout << positives << " ";
+        int j = negatives;
+        while(j < arr.size()){
+            arr.at(j) != 0 ? cout << arr.at(j) << " " : cout << "";
+            j++;
+        }
+        cout << endl;        
+        cout << 1 << " ";
+        cout << arr.at(countNegatives);
+    }
     return 0;
 }
