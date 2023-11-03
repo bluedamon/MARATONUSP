ff5b7f9d3e95b1d821c9c574c4744898b91ae7b4
Damon <damon@usp.br>
2023-11-03 11:36:50 -0300

    CODEFORCES: A - Array

--- array.cpp
+++ array.cpp
@@ -1,4 +1,3 @@
-// #include <bits/stdc++.h>
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -8,57 +7,89 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    vector<int> arr;
-    int countNegatives = 0;
-    int all = n;
-    while(n--){
+    vector<int> pos;
+    vector<int> neg;
+    int i = 0;
+    while(i < n){
         int num;
         cin >> num;
-        if(num < 0) countNegatives++;
-        arr.push_back(num);
+        if(num < 0) neg.push_back(num);
+        else if(num > 0) pos.push_back(num);
+        i++;
     }
-    sort(arr.begin(), arr.end());
-
-    if(countNegatives % 2 == 0){
-        int negatives = countNegatives - 3;
-        if(countNegatives == 2) negatives = 1;
-        cout << negatives << " ";
-        int i = 0;
-        while(i < negatives){
-            cout << arr.at(i) << " ";
-            i++;
+    if(neg.size() == 1){
+        cout << 1 << " " << neg.at(0);
+        cout << endl;
+        int pSize = pos.size();
+        cout << pSize << " ";
+        while(pSize > 0){
+            pSize--;
+            cout << pos.at(pSize) << " ";
         }
         cout << endl;
-        int positives = arr.size() - negatives - 2;
-        cout << positives << " ";
-        int j = negatives + 1;
-        while(j < arr.size()){
-            arr.at(j) != 0 ? cout << arr.at(j) << " " : cout << "";
-            j++;
-        } //-3 -2 0 4 5
-        cout << endl;        
-        cout << arr.size() - negatives - positives << " ";
-        cout << arr.at(countNegatives - 1) << " " << arr.at(countNegatives);
+        cout << 1 << " " << 0;
     } else{
-        int negatives = countNegatives - 2;
-        if(countNegatives == 1) negatives = 1;
-        cout << negatives << " ";
-        int i = 0;
-        while(i < negatives){
-            cout << arr.at(i) << " ";
-            i++;
+        if(neg.size() % 2 == 0){
+            if(pos.size()){
+                int nSize = neg.size() - 1;
+                cout << nSize << " ";
+                while(nSize > 0){
+                    nSize--;
+                    cout << neg.at(nSize) << " ";
+                }
+                cout << endl;
+                int pSize = pos.size();
+                cout << pSize << " ";
+                while(pSize > 0){
+                    pSize--;
+                    cout << pos.at(pSize) << " ";
+                }
+                cout << endl;
+                cout << 2 << " " << 0 << " " << neg.at(neg.size() - 1);
+            } else {
+                int nSize = neg.size() - 3;
+                cout << nSize << " ";
+                while(nSize > 0){
+                    nSize--;
+                    cout << neg.at(nSize) << " ";
+                }
+                cout << endl;
+                cout << 2 << " ";
+                cout << neg.at(neg.size() - 3) << " " << neg.at(neg.size() - 2) << " ";
+                cout << endl;
+                cout << 2 << " " << 0 << " " << neg.at(neg.size() - 1);
+            }
+        } else{
+            if(pos.size()){
+                int nSize = neg.size();
+                cout << nSize << " ";
+                while(nSize > 0){
+                    nSize--;
+                    cout << neg.at(nSize) << " ";
+                }
+                cout << endl;
+                int pSize = pos.size();
+                cout << pSize << " ";
+                while(pSize > 0){
+                    pSize--;
+                    cout << pos.at(pSize) << " ";
+                }
+                cout << endl;
+                cout << 1 << " " << 0;
+            } else{
+                int nSize = neg.size() - 2;
+                cout << nSize << " ";
+                while(nSize > 0){
+                    nSize--;
+                    cout << neg.at(nSize) << " ";
+                }
+                cout << endl;
+                cout << 2 << " ";
+                cout << neg.at(neg.size() - 2) << " " << neg.at(neg.size() - 1) << " ";
+                cout << endl;
+                cout << 1 << " " << 0;
+            }
         }
-        cout << endl;
-        int positives = arr.size() - negatives - 1;
-        cout << positives << " ";
-        int j = negatives;
-        while(j < arr.size()){
-            arr.at(j) != 0 ? cout << arr.at(j) << " " : cout << "";
-            j++;
-        }
-        cout << endl;        
-        cout << 1 << " ";
-        cout << arr.at(countNegatives);
     }
     return 0;
 }
