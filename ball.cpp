3fe40a525d70a3835d5f6ee6b764736bbd33f1c3
Damon <damon@usp.br>
2023-11-11 23:02:41 -0300

    CODEFORCES: A - Jumping Ball

--- ball.cpp
+++ ball.cpp
@@ -1,34 +1,56 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <vector>
-#define FASTIO ios_bas::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
-    int parcial = 1;
+    string input;
+    cin >> input;
+    int i = 0;
+    char first = input[0];
+    char last = input[n - 1];
     int sum = 0;
-    char first;
-    cin >> first;
-    n--;
-    while(n--){
-        char input;
-        cin >> input;
-        if(input == first){
-            parcial++;
-        } else{
-            if(first == '>' && input == '<'){
-                parcial = 0;
-            } else{
-                sum += parcial;
-                parcial = 0;
-                first = input;
+    int parcial = 0;
+    bool onlyLeft = true;
+    bool onlyRight = true;
+    if(first == '>' && last == '<'){
+        cout << 0 << endl;
+        onlyLeft = false;
+        onlyRight = false;
+    }
+    else{
+        if(first == '<'){
+            int i = 0;
+            while(i < n){
+                first = input[i];
+                if(first == '<'){
+                    parcial++;
+                } else{
+                    sum += parcial;
+                    onlyLeft = false;
+                    break;
+                }
+                i++;
             }
         }
+        if(last == '>'){
+            parcial = 0;
+            int i = n;
+            while(i){
+                i--;
+                last = input[i];
+                if(last == '>'){
+                    parcial++;
+                } else{
+                    sum += parcial;
+                    onlyRight = false;
+                    break;
+                }
+            }
+        }      
     }
-    if(parcial) cout << parcial << endl;
-    else cout << sum << endl;
+    if(onlyLeft || onlyRight) cout << parcial << endl;
+    else if(sum != 0) cout << sum << endl;
     return 0;
 }
